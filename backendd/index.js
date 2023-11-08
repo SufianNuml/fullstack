4d777a5ff20e8c88c2bbe1f75056a6b6accd0d4f
Sufian Aslam <sufianaslam127@gmail.com>
2023-11-08 21:32:38 +0500

    Update index.js

--- backendd/index.js
+++ backendd/index.js
@@ -5,6 +5,12 @@ const cors=require("cors");
 
 const app=express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(cors(
     {
          origin: ["https://fullstack-frontend-phi.vercel.app"],
