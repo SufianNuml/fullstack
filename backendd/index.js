1fdde80449d7f943f8249e026136c0066ec3237f
Sufian Aslam <sufianaslam127@gmail.com>
2023-11-09 02:04:59 +0500

    Update index.js

--- backendd/index.js
+++ backendd/index.js
@@ -1,41 +1,27 @@
-const express=require("express");
+const express = require("express");
 require("./db/config");
-const test=require("./db/user")
-const cors=require("cors");
+const test = require("./db/user");
+const cors = require("cors");
 
-const app=express();
-app.use(cors());
-  // {
-  //        origin: ["https://fullstack-frontend-phi.vercel.app"],
-  //   methods: ["POST", "GET"],
-  //    credentials: true
-  //   }
+const app = express();
+app.use(cors({
+    origin: process.env.ALLOWED_ORIGIN,  // Set this as an environment variable
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 app.use(express.json());
 
+app.get("/", (req, resp) => {
+    resp.json("hello");
+});
 
-app.get("/",(req,resp)=>
-{    
-    
-     resp.json("hello");
-})
-
-
-app.post("/login",async(req,resp)=>
-{
-   
-  
-    let data=new test(req.body);
-    let result=await data.save();
-    // result=result.toObject();
-    // delete result.password;
+app.post("/login", async (req, resp) => {
+    let data = new test(req.body);
+    let result = await data.save();
     resp.send(result);
+});
 
-})
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Server is running on port " + port);
 });
-// app.listen(5000 , ()=>
-// {
-//     console.log("good");
-// });
